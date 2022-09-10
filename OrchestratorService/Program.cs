346a96ca369c342a2c9e015ffa44436ff7330614
Lynn Orrell <Lynn.Orrell@microsoft.com>
2022-09-10 01:01:37 -0500

    Initial commit

---
+++ OrchestratorService/Program.cs
@@ -0,0 +1,30 @@
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+using OrchestratorService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        var fullyQualifiedNamespace = Environment.GetEnvironmentVariable("AZURE_SERVICE_BUS_NAMESPACE");
+        if(string.IsNullOrEmpty(fullyQualifiedNamespace))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please ensure the environment variable [AZURE_SERVICE_BUS_NAMESPACE] is set!");
+            System.Environment.Exit(0);
+        }
+
+        var sbAdminClient = new ServiceBusAdministrationClient(fullyQualifiedNamespace, new DefaultAzureCredential());
+        var options = new ServiceBusClientOptions
+        {
+            EnableCrossEntityTransactions = true
+        };
+        var sbClient = new ServiceBusClient(fullyQualifiedNamespace, new DefaultAzureCredential(), options);
+
+        services.AddHostedService<Worker>();
+        services.AddSingleton<ServiceBusAdministrationClient>(implementationInstance: sbAdminClient);
+        services.AddSingleton<ServiceBusClient>(implementationInstance: sbClient);
+    })
+    .Build();
+
+await host.RunAsync();
